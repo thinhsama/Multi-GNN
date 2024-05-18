82a526cb8e18c7e7488872a844f4aff35ff17a52
Nguyễn Trường Thịnh <100020360+thinhsama@users.noreply.github.com>
2024-05-18 22:37:44 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -57,13 +57,7 @@ def infer_gnn(tr_data, val_data, te_data, tr_inds, val_inds, te_inds, args, data
 
     if args.reverse_mp:
         model = to_hetero(model, te_data.metadata(), aggr='mean')
-    
-    if not (args.avg_tps or args.finetune):
-        command = " ".join(sys.argv)
-        name = ""
-        name = '-'.join(name.split('-')[3:])
-        args.unique_name = name
-
+        
     logging.info("=> loading model checkpoint")
     checkpoint = torch.load(f'{data_config["paths"]["model_to_load"]}/checkpoint_{args.unique_name}.tar')
     start_epoch = checkpoint['epoch']
